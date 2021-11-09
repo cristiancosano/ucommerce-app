b708b6dc3162fe0f0414a08d654d86d604343fd5
cristiancosano <cristiancosano@icloud.com>
2021-11-09 15:22:51 +0100

    Add notification badge to cart and shopping history style

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -8,9 +8,14 @@ import { UserService } from '../services/user/user.service';
 })
 export class TabsPage {
 
+  private numCartItems: number
+
   constructor(private userService: UserService) {
+    this.numCartItems = 0
   }
 
   isAuth = () => this.userService.isAuth()
+
+  //TODO: Obtener del servicio de carrito el numero de articulos que hay en la cesta y setearlo en numCartItems.
 
 }
